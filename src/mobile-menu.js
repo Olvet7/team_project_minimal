1b80f4da899139336352d6c3541e28d90cecc967
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-08-26 15:10:02 -0600

    updated JS files

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -12,3 +12,16 @@
     refs.menu.classList.toggle("is-hidden");
   }
 })();
+
+var openBtn = document.getElementById('openBtn');
+var closeBtn = document.getElementById('closeBtn');
+
+openBtn.addEventListener('click', function () {
+  openBtn.classList.add('hidden');
+  closeBtn.classList.remove('hidden');
+});
+
+closeBtn.addEventListener('click', function () {
+  closeBtn.classList.add('hidden');
+  openBtn.classList.remove('hidden');
+});
