db3778bde42c5fdfce02cac50b9f6a782d452c4d
Escbrk <69437294+Escbrk@users.noreply.github.com>
2023-08-26 20:16:50 -0600

    add additional JS code

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -17,11 +17,50 @@ var openBtn = document.getElementById('openBtn');
 var closeBtn = document.getElementById('closeBtn');
 
 openBtn.addEventListener('click', function () {
-  openBtn.classList.add('hidden');
-  closeBtn.classList.remove('hidden');
+  openBtn.classList.toggle('hidden');
+  closeBtn.classList.toggle('hidden');
 });
 
 closeBtn.addEventListener('click', function () {
-  closeBtn.classList.add('hidden');
-  openBtn.classList.remove('hidden');
+  closeBtn.classList.toggle('hidden');
+  openBtn.classList.toggle('hidden');
+});
+
+var is_hidden = document.getElementById('is_hidden');
+var about_us_anchor = document.getElementById('about_us_anchor');
+var our_products_anchor = document.getElementById('our_products_anchor');
+var prices_anchor = document.getElementById('prices_anchor');
+var bestsellers_anchor = document.getElementById('bestsellers_anchor');
+var reviews_anchor = document.getElementById('reviews_anchor');
+var contacts_anchor = document.getElementById('contacts_anchor');
+
+about_us_anchor.addEventListener('click', function () {
+  is_hidden.classList.toggle('is-hidden');
+  openBtn.classList.toggle('hidden');
+  closeBtn.classList.toggle('hidden');
+});
+our_products_anchor.addEventListener('click', function () {
+  is_hidden.classList.toggle('is-hidden');
+  openBtn.classList.toggle('hidden');
+  closeBtn.classList.toggle('hidden');
+});
+prices_anchor.addEventListener('click', function () {
+  is_hidden.classList.toggle('is-hidden');
+  openBtn.classList.toggle('hidden');
+  closeBtn.classList.toggle('hidden');
+});
+bestsellers_anchor.addEventListener('click', function () {
+  is_hidden.classList.toggle('is-hidden');
+  openBtn.classList.toggle('hidden');
+  closeBtn.classList.toggle('hidden');
+});
+reviews_anchor.addEventListener('click', function () {
+  is_hidden.classList.toggle('is-hidden');
+  openBtn.classList.toggle('hidden');
+  closeBtn.classList.toggle('hidden');
+});
+contacts_anchor.addEventListener('click', function () {
+  is_hidden.classList.toggle('is-hidden');
+  openBtn.classList.toggle('hidden');
+  closeBtn.classList.toggle('hidden');
 });
